0809dc259ca3f4e59fac30464d1789fdad5be2ab
rohithaider <rohithaider@gmail.com>
2024-11-01 03:18:43 +0600

    Project setup

---
+++ src/App.jsx
@@ -0,0 +1,4 @@
+export default function App(){
+    return (
+        <h1 className="bg-red-500">hello</h1>
+    );}
---
+++ src/components/root/Root.jsx
@@ -0,0 +1,6 @@
+export default function Root(){
+    return (
+        <h3>Hello from root</h3>
+        
+    );
+}
